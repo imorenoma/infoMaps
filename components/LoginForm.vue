9071fc074a67ab6a8db6faded8b7d9b27c1aaed5
ivan <iv.morenoma@gmail.com>
2024-02-23 19:46:43 +0100

    void file

--- components/LoginForm.vue
+++ components/LoginForm.vue
@@ -1,49 +1,8 @@
 <template>
     <div>
-      <h2>Login</h2>
-      <form @submit.prevent="login">
-        <div class="form-group">
-          <label for="email">Email</label>
-          <input type="email" id="email" v-model="email" required>
-        </div>
-        <div class="form-group">
-          <label for="password">Password</label>
-          <input type="password" id="password" v-model="password" required>
-        </div>
-        <button type="submit">Login</button>
-      </form>
+      
     </div>
   </template>
   
   <script>
-  import axios from 'axios';
   
-  export default {
-    data() {
-      return {
-        email: '',
-        password: ''
-      };
-    },
-    methods: {
-      async login() {
-        try {
-          const response = await axios.post('http://localhost:5000/login', {
-            email: this.email,
-            password: this.password
-          });
-          console.log(response.data);
-        } catch (error) {
-          console.error(error);
-        }
-      }
-    }
-  };
-  </script>
-  
-  <!-- TODO: move style code to a new file css and import here -->
-  <style scoped>
-  .form-group {
-    margin-bottom: 1rem;
-  }
-  </style>
