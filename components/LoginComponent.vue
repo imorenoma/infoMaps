c29360aa1c3a701fe94134696000232c9fd9733d
ivan <iv.morenoma@gmail.com>
2024-02-23 17:36:50 +0100

    this component imports register form and login form

---
+++ components/LoginComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="max-w-screen-xl mx-auto px-5">
+    <login-form></login-form>
+    <register-form></register-form>
+  </div>
+</template>
+
+
+<script>
+import LoginForm from './LoginForm.vue'
+import RegisterForm from './RegisterForm.vue'
+
+export default{
+  components:{
+    LoginForm,
+    RegisterForm
+  }
+};
+
+</script>
