6ed7325bd1df0cce486ab24ee82279325cb01cca
Alvaro <sali.alvaro@gmail.com>
2024-02-29 18:09:49 +0100

    Filtrado de marcadores en mapa y de tabla funcional

--- components/MapComponent.vue
+++ components/MapComponent.vue
@@ -5,28 +5,124 @@ export default {
     setup: function () {
         return {
             google_maps_url: '',
-            places: [],
-            selectedFilter: 'todos',
             map: null,
             markers: []
         }
     },
     data: function () {
-
+        return {
+            places: [],
+            selectedFilter: 'todos',
+            
+        }
     },
     computed: {
 
         filteredPlaces() {
             if (this.selectedFilter === 'todos') {
+
                 return this.places;
             }
+            else{
+
             return this.places.filter(place => place.type === this.selectedFilter);
+            }
         },
     },
     async created() {
         //const gResponse = await fetch("http://localhost:5000/citysights");
         // const gObject = await gResponse.json();
         this.google_maps_url = "https://maps.googleapis.com/maps/api/js?key=AIzaSyC-xhrnV-K304RreVro53FFhm6KIjKw3rY";
+        this.myPlaces =  [
+	
+	{
+		"@id": "https://datos.madrid.es/egob/catalogo/tipo/entidadesyorganismos/6017808-casa-museo-fuente-rey.json",
+		"@type": "https://datos.madrid.es/egob/kos/entidadesYorganismos/FundacionesCulturales",
+		"id": "6017808",
+		"title": "Casa Museo Fuente del Rey",
+		"relation": "http://www.madrid.es/sites/v/index.jsp?vgnextchannel=bfa48ab43d6bb410VgnVCM100000171f5a0aRCRD&vgnextoid=5c1260101310f210VgnVCM1000000b205a0aRCRD",		
+		"address": {
+			"district": {
+				"@id": "https://datos.madrid.es/egob/kos/Provincia/Madrid/Municipio/Madrid/Distrito/Moncloa-Aravaca"
+			},
+			"area": {
+				"@id": "https://datos.madrid.es/egob/kos/Provincia/Madrid/Municipio/Madrid/Distrito/Moncloa-Aravaca/Barrio/Aravaca"
+			},
+			"locality": "MADRID",			
+			"postal-code": "28023",
+			"street-address": "CALLE FUENTE DEL REY 11"
+		},
+		"location": {
+			"latitude": 40.45546866305681,
+			"longitude": -3.7705716814130965
+		},
+		"organization": {
+			"organization-desc": "Fundación AMYC (Arte Moderno y Contemporáneo).   Este museo, situado en Aravaca y conocido también como Casa - Museo Fuente del Rey. Fundación AMYC, ofrece al visitante una colección de más de 250 obras entre pinturas, dibujos y esculturas (198 pinturas, 37 esculturas y 27 dibujos) realizadas desde mediados del siglo XIX hasta principios del XXI. Se exponen piezas de grandes artistas modernistas e impresionistas como Sorolla, Mariano Fortuny, Nonell, Rusiñol, Ramón Casas, Mir, Rendir, Gargallo, Joseph Llimona, Joan Miró, Salvador Dalí, Tapies, Anglada Camarasa o Torres García, entre otros. La colección está formada por los fondos privados del coleccionista y mecenas Francisco Daurella y se muestra en lo que fue su casa. Los más de 3000 metros cuadrados del edificio así como su jardín, han sido rehabilitados para poder acoger las obras en el que es el primer museo de la capital dedicado en exclusiva al arte catalán.  Bus: 161.",
+			"accesibility": "3",
+			"schedule": "Martes a sábado, de 10 a 19 horas. Lunes, domingos y festivos cerrado. Visitas para grupos de hasta 25 personas dentro de los horarios de apertura. Pueden ser con guía propio o con guía de la Fundación, y la duración de la misma es de una hora, previa reserva telefónica o por email.",
+			"services": "",
+			"organization-name": "Casa Museo Fuente del Rey"
+		}
+	},
+	{
+		"@id": "https://datos.madrid.es/egob/catalogo/tipo/entidadesyorganismos/4847190-casa-museo-lope-vega.json",
+		"@type": "https://datos.madrid.es/egob/kos/entidadesYorganismos/Museos",
+		"id": "4847190",
+		"title": "Casa Museo Lope de Vega",
+		"relation": "http://www.madrid.es/sites/v/index.jsp?vgnextchannel=bfa48ab43d6bb410VgnVCM100000171f5a0aRCRD&vgnextoid=4eff8c46145e8110VgnVCM2000000c205a0aRCRD",		
+		"address": {
+			"district": {
+				"@id": "https://datos.madrid.es/egob/kos/Provincia/Madrid/Municipio/Madrid/Distrito/Centro"
+			},
+			"area": {
+				"@id": "https://datos.madrid.es/egob/kos/Provincia/Madrid/Municipio/Madrid/Distrito/Centro/Barrio/Cortes"
+			},
+			"locality": "MADRID",			
+			"postal-code": "28014",
+			"street-address": "CALLE CERVANTES 11"
+		},
+		"location": {
+			"latitude": 40.41436862259971,
+			"longitude": -3.6974667215860615
+		},
+		"organization": {
+			"organization-desc": "'Mi casilla, mi quietud, mi g&amp;uuml;ertecillo y estudio'. Así define su casa Félix Lope de Vega (1562-1635) en una carta dirigida a un amigo. Situada en pleno centro histórico, en el Barrio de las Letras, la Casa Museo Lope de Vega se ubica en el edificio donde el escritor vivió sus últimos 25 años. La recreación de ambientes, cuyo objetivo es que se respire la presencia de Lope, evoca la vida cotidiana del Siglo de Oro y nos acerca a su intimidad. El equipamiento de la casa incorpora obras de arte, mobiliario, enseres y ediciones bibliográficas vinculadas al literato y su tiempo.  Metro: Antón Martín (línea 1), Sevilla (línea 2), Sol (líneas 1, 2 y 3).  Bus: 002, M1, 001, 10, 14, 27, 34, 37, 45, C03, 26, 32, 6, 51,   Cercanías Renfe:  Sol (líneas C3 y C4).   Bicimad: Estaciones 27 (calle Jesús), 52 (plaza de Santa Ana, 10)  Aparcamientos: Cortes (46), plaza de las Cortes; plaza de Santa Ana (77).",
+			"accesibility": "0,4",
+			"schedule": "De martes a domingo, de 10 a 18 horas.  Cerrado los lunes, días 1 y 6 de enero, 1 de mayo, 9 de noviembre y 24, 25 y 31 de diciembre. El acceso al museo se hace mediante visitas guiadas en grupo. Es imprescindible reservar con antelación, por teléfono o correo electrónico.",
+			"services": "",
+			"organization-name": "Casa Museo Lope de Vega"
+		}
+	},
+	{
+		"@id": "https://datos.madrid.es/egob/catalogo/tipo/entidadesyorganismos/4949641-casita-museo-raton-perez.json",
+		"@type": "https://datos.madrid.es/egob/kos/entidadesYorganismos/Museos",
+		"id": "4949641",
+		"title": "Casita - Museo del Ratón Pérez",
+		"relation": "http://www.madrid.es/sites/v/index.jsp?vgnextchannel=bfa48ab43d6bb410VgnVCM100000171f5a0aRCRD&vgnextoid=990950e86d4ca110VgnVCM2000000c205a0aRCRD",		
+		"address": {
+			"district": {
+				"@id": "https://datos.madrid.es/egob/kos/Provincia/Madrid/Municipio/Madrid/Distrito/Centro"
+			},
+			"area": {
+				"@id": "https://datos.madrid.es/egob/kos/Provincia/Madrid/Municipio/Madrid/Distrito/Centro/Barrio/Sol"
+			},
+			"locality": "MADRID",			
+			"postal-code": "28013",
+			"street-address": "CALLE ARENAL 8 PLANTA 1.ª"
+		},
+		"location": {
+			"latitude": 40.4170597715272,
+			"longitude": -3.7055199334442492
+		},
+		"organization": {
+			"organization-desc": "La Casita-Museo de Ratón Pérez, constituye un singular lugar que da la bienvenida a cuantos visitantes allí acuden con el deseo de conocer y encontrarse con este extraordinario, único, madrileño y universal ratón. Se aconseja acudan con la entrada adquirida anticipadamente.   En  taquilla.  Whatsapp (634 74 27 68)  No se reservan entradas por correo electrónico, toda información que necesite sobre entradas podrá hacerlo vía whatsapp. Se aconseja reservar con antelación, ESPECIALMENTE los FINES DE SEMANA. Metro: Sol (líneas: 1, 2 y 3), Ópera (líneas 2, 5 y Ramal Ópera Príncipe Pío), Callao (líneas 3, 5).  Bus: M3.  Cercanías Renfe: Sol (líneas C3, C4, C4a, C4b y Regional).  Bicimad: Estaciones 1 (calle Mayor, 6), 31 (calle Mayor, 16), 25 a y b (Plaza Celenque, 1).  Aparcamientos: El Core Inglés Preciados (93) calle Preciados; Descalzas (20) plaza Descalzas y plaza San Martín; plaza del Carmen (8); plaza Mayor (55).",
+			"accesibility": "0",
+			"schedule": "Visitas familiares:    Lunes a viernes:  de 11 a 14 y de 17 a 20 horas Sábados domingos y festivos: de  11 a 15 y dec16 a 20 horas (Cerrado 25 de diciembre, 1 y 6 de enero, 1 de mayo)   Visitas escolares:   Información y reservas: Lunes a Viernes 09 a 14 horas. Teléfono exclusivo para atender colegios:  607 67 97 75 (solo se atenderán llamadas de peticiones de colegios o asociaciones)",
+			"services": "",
+			"organization-name": "Casita - Museo del Ratón Pérez"
+		}
+	},
+	];
         this.places = [
             {
                 id:1,
@@ -55,13 +151,6 @@ export default {
 
         await this.initGoogleMaps();
 
-    },
-    watch: {
-        selectedFilter() {
-            // Update markers when the filter changes
-            this.clearMarkers();
-            this.addMarkers(this.map);
-        }
     },
     methods: {
         async initGoogleMaps() {
@@ -160,13 +249,13 @@ export default {
                 },
             };
             const vm = this;
-
+            //this.clearMarkers();
             for (const  place of this.filteredPlaces) {
                 const content = this.buildContent(place);
                 const marker = new AdvancedMarkerElement({
                     position: place.position,
                     // icon: icons[place.type].icon,
-                    map: mapa,
+                    map: this.map,
                     content: content,
                     title: place.description
                 });
@@ -178,17 +267,6 @@ export default {
             }
         },
 
-        filterPlaces(type) {
-            console.log("Filtrando sitios...");
-            // Example filtering function by type
-            // You can extend this to filter by other parameters
-            const filteredPlaces = this.places.filter(place => place.type === type);
-            // Clear existing markers and add new ones based on the filter
-            // This is a simplistic approach; optimizations may be needed for performance
-            this.clearMarkers();
-            //this.places = filteredPlaces;
-            this.addMarkers();
-        },
         centerMapOnMarker(place) {
             console.log("Centrando en el mapa...");
             const marker = this.markers.find(marker => marker.placeId === place.id);
@@ -201,24 +279,37 @@ export default {
     }
         },
         clearMarkers() {
-            console.log("Despejando filtros...");
             if (this.markers) {
                 for (const marker of this.markers) {
-                    marker.setMap(null); // Removes the marker from the map
+                    marker.setMap(null);
                 }
             }
-            this.markers = []; // Reset the markers array
+            this.markers = [];
         }
     },
-
+    watch: {
+        selectedFilter: function (selectedFilter) {
+            if (selectedFilter == 'todos') {
+                this.clearMarkers();
+                this.addMarkers(this.map);
+                return this.places;
+            }
+            else{
+            this.clearMarkers();
+            this.addMarkers(this.map);
+            return this.places.filter(place => place.type === this.selectedFilter);
+    }
+        }
+    }
 }
+
 </script>
 <template>
     <div>
         <div id="googlemap" style="height: 600px;"></div>
         <div>
             <!-- Filters -->
-            <select v-model="selectedFilter">
+            <select v-model="selectedFilter" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                 <option value="todos">Todos</option>
                 <option value="casa">Casa</option>
                 <option value="biblioteca">Bibliotecas</option>
